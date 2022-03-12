9e35b539a69c26ee82bde6a5bb5924be4dff9544
Trey Dobbs <58719746+TreyDobbs@users.noreply.github.com>
2022-03-11 18:32:58 -0600

    Update Elevator.h

--- Elevator.h
+++ Elevator.h
@@ -1,8 +1,5 @@
 // --------------------------------------------------------------------------
 // Name: Trey Dobbs
-// Course-Section: CS 255-01
-// Assignment: Project #7
-// Date Due: 12/2/2020
 // Description: This C++ project implements all the required pieces for the 
 // SimBuilding class to build a building elevator simulation. Creation of the
 // class constructor, the step method, and the printServiceStats() method are
